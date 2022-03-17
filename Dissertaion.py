7754224b6f13679837dc6da6032d10f06112316d
Aredo-A <101798249+Aredo-A@users.noreply.github.com>
2022-03-17 14:03:55 +0300

    Update Dissertaion.py

--- Dissertaion.py
+++ Dissertaion.py
@@ -26,7 +26,7 @@ df
 type(df)
 
 st.write(""" ### Data Analysis """)
-#fig, axes = plt.subplots(1, 2, sharex=True)
+fig, axes = plt.subplots()
 #axes[0].plot(df.infection_rate);
 #axes[0].set_title('Original Series')
 #plot_acf(df.infection_rate, ax=axes[1])
