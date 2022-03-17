4f0620c3284ff51d6257d2ee91d983d166d5b336
Aredo-A <101798249+Aredo-A@users.noreply.github.com>
2022-03-17 13:53:43 +0300

    Update Dissertaion.py

--- Dissertaion.py
+++ Dissertaion.py
@@ -27,19 +27,8 @@ type(df)
 
 st.write(""" ### Data Analysis """)
 fig, axes = plt.subplots(1, 2, sharex=True)
-axes[0].plot(df.infection_rate);
-axes[0].set_title('Original Series')
-plot_acf(df.infection_rate, ax=axes[1])
+#axes[0].plot(df.infection_rate);
+#axes[0].set_title('Original Series')
+#plot_acf(df.infection_rate, ax=axes[1])
 st.pyplot(fig)
 
-st.write(""" Description of the data and why use first and 
-second order diffrencaition""")
-fig1, axes = plt.subplots(2, 2, sharex=True)
-# 1st Differencing
-axes[0, 0].plot(df.infection_rate.diff()); axes[0, 0].set_title('1st Order Differencing')
-plot_acf(df.infection_rate.diff().dropna(), ax=axes[0, 1])
-
-# 2nd Differencing
-axes[1, 0].plot(df.infection_rate.diff().diff()); axes[1, 0].set_title('2nd Order Differencing')
-plot_acf(df.infection_rate.diff().diff().dropna(), ax=axes[1, 1])
-st.pyplot(fig1)
