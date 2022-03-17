2f8ae30d0392f3fd59426acc7c610c571dd33d5a
Aredo-A <101798249+Aredo-A@users.noreply.github.com>
2022-03-17 13:26:04 +0300

    Update Dissertaion.py

--- Dissertaion.py
+++ Dissertaion.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import altair as alt
-import yfinance as yf
 import streamlit as st
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import plot , draw, show
