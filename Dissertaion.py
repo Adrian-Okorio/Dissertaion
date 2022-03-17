31ca17daf9ac2eb5048c59348a074a3d500e8193
Aredo-A <101798249+Aredo-A@users.noreply.github.com>
2022-03-17 19:52:14 +0300

    Update Dissertaion.py

--- Dissertaion.py
+++ Dissertaion.py
@@ -15,6 +15,9 @@ if st.button('Original_Data'):
      original = pd.read_csv("https://raw.githubusercontent.com/Aredo-A/Dissertaion/main/original.csv")
      original
      type(original)
+ st.write("""  imaage description of the most affected areas in udanda """)
+st.write(""" data collected description""")
+st.image("https://raw.githubusercontent.com/Aredo-A/Dissertaion/main/diagram.jpg")
 
 st.write("""  Below is data that was extracted after the data mining steps
  were taken which include
@@ -31,3 +34,17 @@ axes[0].plot(df.infection_rate);
 axes[0].set_title('Original Series')
 plot_acf(df.infection_rate, ax=axes[1])
 st.pyplot(fig)
+
+
+st.write("""  auto correlation description """)
+st.write(""" Description of the data and why use first and 
+second order diffrencaition""")
+fig1, axes = plt.subplots(2, 2, sharex=True)
+# 1st Differencing
+axes[0, 0].plot(df.infection_rate.diff()); axes[0, 0].set_title('1st Order Differencing')
+plot_acf(df.infection_rate.diff().dropna(), ax=axes[0, 1])
+
+# 2nd Differencing
+axes[1, 0].plot(df.infection_rate.diff().diff()); axes[1, 0].set_title('2nd Order Differencing')
+plot_acf(df.infection_rate.diff().diff().dropna(), ax=axes[1, 1])
+st.pyplot(fig1)
