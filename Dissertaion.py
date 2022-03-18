da0f543724641911a5b851df97a40a3dc7c97113
Adrian <101798249+Aredo-A@users.noreply.github.com>
2022-03-18 11:22:50 +0300

    Update Dissertaion.py

--- Dissertaion.py
+++ Dissertaion.py
@@ -47,3 +47,78 @@ plot_acf(df.infection_rate.diff().dropna(), ax=axes[0, 1])
 axes[1, 0].plot(df.infection_rate.diff().diff()); axes[1, 0].set_title('2nd Order Differencing')
 plot_acf(df.infection_rate.diff().diff().dropna(), ax=axes[1, 1])
 st.pyplot(fig1)
+
+st.write("""  identify if the model needs any AR terms""")
+st.write("""Partial autocorrelation of lag (k) of a series is the coefficient of that lag in the autoregression equation of Y.
+""")
+st.image("https://www.machinelearningplus.com/wp-content/uploads/2021/06/arima-equation.jpg")
+
+
+
+fig8, axes = plt.subplots(1, 2, sharex=True)
+axes[0].plot(df.infection_rate.diff()); axes[0].set_title('1st Differencing')
+axes[1].set(ylim=(0,5))
+plot_pacf(df.infection_rate.diff().dropna(), lags= 10, ax=axes[1])
+
+st.pyplot(fig8)
+
+
+
+
+st.write("""The ACF tells how many MA terms are required to remove any autocorrelation in the stationarized series.
+Let’s see the autocorrelation plot of the differenced series.""")
+fig6, axes = plt.subplots(1, 2, sharex=True)
+axes[0].plot(df.infection_rate.diff()); axes[0].set_title('1st Differencing')
+axes[1].set(ylim=(0,1.2))
+plot_acf(df.infection_rate.diff().dropna(), ax=axes[1])
+st.pyplot(fig6)
+
+
+st.write(""" ### Building the ARIMA Model""")
+model = ARIMA(df.infection_rate, order=(1,1,1))
+model_fit = model.fit()
+st.write(model_fit.summary())
+
+
+# Plot residual errors
+residuals = pd.DataFrame(model_fit.resid)
+fig3, ax = plt.subplots(1,2)
+residuals.plot(title="Residuals", ax=ax[0])
+residuals.plot(kind='kde', title='Density', ax=ax[1])
+st.pyplot(fig3)
+
+#---------------------------------------------------------------
+
+# Create Training and Test
+train = df.infection_rate[:15]
+test = df.infection_rate[15:]
+
+
+# Build Model
+model = ARIMA(train, order=(3, 2, 1))
+fitted = model.fit()
+st.write(fitted.summary())
+
+
+
+fig4, axes = plt.subplots(1, sharex=True)
+axes.plot(train);
+axes.plot(test)
+axes.set_title('Forecast')
+
+st.pyplot(fig4)
+
+fc = fitted.forecast(15, alpha=0.05)
+fc
+fc_series = pd.Series(fc, index=test.index)
+lower_series = pd.Series(fc[:20], index=test.index)
+upper_series = pd.Series(fc[20:], index=test.index)
+
+fig5, axes = plt.subplots(1, sharex=True)
+axes.plot(train, label='training')
+axes.plot(test, label='actual')
+axes.plot(fc_series, label='forecast')
+axes.fill_between(lower_series.index, lower_series, upper_series,
+                 color='k', alpha=.15)
+axes.legend(loc='upper left', fontsize=8)
+st.pyplot(fig5)
